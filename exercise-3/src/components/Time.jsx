7a5040eda5461afd69745c874b362a32093ae547
Hengsreng2409 <hengsreng.toek@student.cadt.edu.kh>
2025-02-25 21:06:27 +0700

    Add files via upload

---
+++ exercise-3/src/components/Time.jsx
@@ -0,0 +1,11 @@
+function Time() {
+    let date = new Date().toLocaleString() + ""
+    let time = new Date().toLocaleTimeString() + ""
+    return(
+        <h2>
+            {time}
+        </h2>
+    );
+}
+
+export default Time;
